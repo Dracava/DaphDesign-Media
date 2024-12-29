09604d65907377a52d78f3d124cd07f05927d211
Dracava <50298531+Dracava@users.noreply.github.com>
2024-12-29 18:46:49 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -157,3 +157,35 @@ for (let i = 0; i < navigationLinks.length; i++) {
 
   });
 }
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// Auto advance slides
+setInterval(function() {
+  slideIndex++;
+  showSlides(slideIndex);
+}, 5000); // Change slide every 5 seconds
